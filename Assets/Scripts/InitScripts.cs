857f0d17bb2edcca9a55576f11a26850dfbd8c08
微风的龙骑士 风游迩 <2944262241@qq.com>
2018-07-11 22:59:45 +0800

    1

---
+++ Assets/Scripts/InitScripts.cs
@@ -0,0 +1,29 @@
+﻿//标题：
+//替换代码注释
+
+//注意：这个脚本没有必要执行！！！
+
+
+//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.IO;
+//using UnityEditor;
+//using UnityEngine;
+
+//public class InitScripts : UnityEditor.AssetModificationProcessor {
+
+//	private const string NAME_Author = "微风的龙骑士";
+
+//	private static void OnWillCreateAsset(string path){
+//		path = path.Replace(".meta", "");
+//		if (path.EndsWith(".cs")) {
+//			string strContent = File.ReadAllText(path);
+//			strContent = strContent.Replace("#AuthorName#", "ss")
+//				.Replace("#CreateTime#", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+//			File.WriteAllText(path,strContent);
+//			AssetDatabase.Refresh();
+//		}
+//	}
+
+//}
